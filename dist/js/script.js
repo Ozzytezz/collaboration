87c08b53050c69a2282dc2a56dd2a3e74156ba5e
Ozzytezz <samillano.nathankurt08@gmail.com>
2024-08-29 13:53:12 +0800

    wawa

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,13 +1,17 @@
 const burgirBtn = document.querySelector('.burgir');
 const headerNav = document.querySelector('.link');
 const kamote = document.querySelector('.kamote');
+const dropdown = document.querySelector(".fa-solid.fa-circle-plus");
 
 
 burgirBtn.addEventListener("click", () => {    
     burgirBtn.classList.toggle('open');
     headerNav.classList.toggle('open');
     kamote.classList.toggle('open');
+    dropdown.classList.toggle('open');
 })
+
+
 
 const header = document.querySelector(".header");
 window.addEventListener("scroll" , () => {
@@ -17,5 +21,6 @@ window.addEventListener("scroll" , () => {
 } else {
   header.classList.remove("blue")
 }
-
 });
+
+const fontcolor = document.querySelector("a");
